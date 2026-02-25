72ce14daff5754b2c56568c4c224020a265ecf35
techizone <venkateshsap413@gmail.com>
2026-02-25 16:33:08 +0530

    Added Project files

--- backend/src/main/java/com/emart/login/exception/InvalidCredentialsException.java
+++
@@ -1,4 +0,0 @@
-package com.emart.login.exception;
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) { super(message); }
-}
--- backend/src/main/java/com/emart/login/exception/ValidationException.java
+++
@@ -1,4 +0,0 @@
-package com.emart.login.exception;
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) { super(message); }
-}
